993a7b5cb0bfa15a6c98d728d7f90dd95e0338ca
Yaeko Ogino <33792095+yaechrome@users.noreply.github.com>
2018-06-13 12:35:44 -0400

    se actualiza usuarioDaoImp

--- dao/UsuarioDaoImp.php
+++ dao/UsuarioDaoImp.php
@@ -1,16 +1,22 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- * Description of UsuarioDaoImp
- *
- * @author CETECOM
- */
-class UsuarioDaoImp {
-    //put your code here
+class UsuarioDaoImp implements UsuarioDao{
+    
+    public function agregar($dto) {
+        
+    }
+
+    public function existeRegistro($key) {
+        
+    }
+
+    public function listar() {
+        
+    }
+
+    public function modificar($dto) {
+        
+    }
+
 }
