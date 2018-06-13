6091c50af7144edbf76a260eda2a097535425065
Yaeko Ogino <33792095+yaechrome@users.noreply.github.com>
2018-06-13 14:25:03 -0400

    Se agregan páginas pero falta crearles el body y sus respectivos servlets

---
+++ paginas/ventanaAgregarUsuario.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="agregarUsuario.php" method="POST">
+            <table border="0">
+                <tbody>
+                    <tr>
+                        <td>Rut</td>
+                        <td><input type="text" name="txtRut" value="" /></td>
+                    </tr>
+                    <tr>
+                        <td>Nombre</td>
+                        <td><input type="text" name="txtNombre" value="" /></td>
+                    </tr>
+                    <tr>
+                        <td>Apellido Paterno</td>
+                        <td><input type="text" name="txtApellidoPaterno" value="" /></td>
+                    </tr>
+                    <tr>
+                        <td>Apellido Materno</td>
+                        <td><input type="text" name="txtApellidoMaterno" value="" /></td>
+                    </tr>
+                    <tr>
+                        <td>Contraseña</td>
+                        <td><input type="password" name="txtContraseña" value="" /></td>
+                    </tr>
+                    <tr>
+                        <td>Repetir Contraseña</td>
+                        <td><input type="password" name="txtContraseña" value="" /></td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </form>
+    </body>
+</html>
