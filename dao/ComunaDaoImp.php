214c418496603625266c9e2033c6179467ce5f1d
Yaeko Ogino <33792095+yaechrome@users.noreply.github.com>
2018-06-13 13:53:34 -0400

    Se actualiza ComunaDaoImp

--- dao/ComunaDaoImp.php
+++ dao/ComunaDaoImp.php
@@ -4,11 +4,42 @@
 class ComunaDaoImp implements BaseDao{
     
     public function listar() {
-        
+        $lista = new ArrayObject();
+        try {
+            $pdo= new clasePDO();
+            $stmt = $pdo->prepare("SELECT * FROM comuna");
+            $stmt->execute();
+            $resultado = $stmt->fetchAll();
+            foreach ($resultado as $value) {
+                $dto = new ComunaDto();
+                $dto->setId($value["id"]);
+                $dto->setDescripcion($value["descripcion"]);
+                $lista->append($dto);                
+            }
+            $pdo=NULL;
+        } catch (Exception $exc) {
+            echo "Error dao al listar ".$exc->getMessage();
+        }
+        return $lista;
     }
 
     public function buscarPorClavePrimaria($id) {
-        
+        $comuna = new ComunaDto();
+        try {
+            $pdo= new clasePDO();
+            $stmt = $pdo->prepare("SELECT * FROM comuna where id=?");
+            $stmt->bindParam(1, $id);
+            $stmt->execute();
+            $resultado = $stmt->fetchAll();
+            foreach ($resultado as $value) {
+                $comuna->setId($value["id"]);
+                $comuna->setDescripcion($value["descripcion"]);
+            }
+            $pdo=NULL;
+        } catch (Exception $exc) {
+            echo "Error dao al buscar por id ".$exc->getMessage();
+        }
+        return $comuna;
     }
 
 }
