fea49ce90f89eccc0f69087c495d8c2f9a1576ce
Yaeko Ogino <yaekoogino@gmail.com>
2018-06-18 17:36:55 -0400

    Se agrega buscarSolicitudPorFecha

--- paginas/buscarSolicitudPorFecha.php
+++ paginas/buscarSolicitudPorFecha.php
@@ -1,2 +1,12 @@
 <?php
 
+include_once '../dao/PostulacionDaoImp.php';
+
+$desde = $_POST["txtDesde"];
+$hasta = $_POST["txtHasta"];
+$dao = new PostulacionDaoImp();
+$lista = $dao->buscarPorFecha($desde,$hasta);
+session_start();
+$_SESSION["listaDesplegar"]= $lista;
+
+include_once 'ventanaBuscarSolicitud.php';
