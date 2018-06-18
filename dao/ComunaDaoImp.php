48a0a3d524919ca07359dd36015dec9cdb4f7ce7
Joselyn Moya <joselyn.moyap@gmail.com>
2018-06-18 17:21:30 -0400

    Se crea buscarPorNombre($nombre)

--- dao/ComunaDaoImp.php
+++ dao/ComunaDaoImp.php
@@ -2,8 +2,9 @@
 include_once '../bd/ClasePDO.php';
 include_once '../dto/ComunaDto.php';
 include_once 'BaseDao.php';
+include_once 'ComunaDao.php';
 
-class ComunaDaoImp implements BaseDao{
+class ComunaDaoImp implements ComunaDao{
     
     public static function listar() {
         $lista = new ArrayObject();
@@ -33,6 +34,25 @@ class ComunaDaoImp implements BaseDao{
             foreach ($resultado as $value) {
                 $comuna->setId($value["id"]);
                 $comuna->setDescripcion($value["descripcion"]);
+            }
+            $pdo=NULL;
+        } catch (Exception $exc) {
+            echo "Error dao al buscar por id ".$exc->getMessage();
+        }
+        return $comuna;
+    }
+    
+    public function buscarPorNombre($nombre){
+        
+        $comuna = null;
+        try {
+            $pdo= new clasePDO();
+            $stmt = $pdo->prepare("select id from comuna where nombre=?");
+            $stmt->bindParam(1, $nombre);
+            $stmt->execute();
+            $resultado = $stmt->fetchAll();
+            foreach ($resultado as $value) {
+                $comuna= $value["descripcion"];
             }
             $pdo=NULL;
         } catch (Exception $exc) {
