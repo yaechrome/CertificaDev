ac203af9d549e2eb984fa636ad81881f2c7698ac
Yaeko Ogino <yaekoogino@gmail.com>
2018-06-19 22:32:43 -0400

    limpieza de código

--- login/panel-control.php
+++ login/panel-control.php
@@ -1,60 +1,39 @@
 <?php
-//session_start();
+$is_logged_in = isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true;
+if (!$is_logged_in) {
+    echo "Esta pagina es solo para usuarios registrados.<br>";
+    echo "<br><a href='login.html'>Login</a>";
 
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-
-} else {
-   echo "Esta pagina es solo para usuarios registrados.<br>";
-   echo "<br><a href='login.html'>Login</a>";
-   //echo "<br><br><a href='index.html'>Registrarme</a>";
-
-exit;
+    exit;
 }
 
 $now = time();
+if ($now > $_SESSION['expire']) {
+    session_destroy();
 
-if($now > $_SESSION['expire']) {
-session_destroy();
-
-echo "Su sesion a terminado,
-<a href='login.html'>Necesita Hacer Login</a>";
-exit;
+    echo "Su sesion a terminado, <a href='login.html'>Necesita Hacer Login</a>";
+    exit;
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+        <title>Menu</title>
+    </head>
+    <body>
+        <h1>Menu</h1>
+        <ul>
+            <?php if ($_SESSION['perfil'] == 'Postulante') { ?>
+                <p><a href="../paginas/validarPostulante.php" >Crear Postulacion</a></p>
+                <p><a href="../paginas/ventanaBuscarPorRut.php" >Estado de Postulacion</a></p>
+            <?php } else { ?>
+                <p><a href="../paginas/validarPostulante.php" >Crear Postulacion</a></p>
+                <p><a href="../paginas/ventanaBuscarSolicitud.php" >Buscar Postulacion</a></p>
+            <?php } ?>
+        </ul>
 
-<head>
-<title>Menu</title>
-</head>
-
-<body>
-<h1>Menu</h1>
-
-
-<ul>
-    
-  <?php 
-    if($_SESSION['perfil']=='Postulante'){?>
-    <p><a href="../paginas/validarPostulante.php" >Crear Postulacion</a></p>
-  <p><a href="../paginas/ventanaBuscarPorRut.php" >Estado de Postulacion</a></p>
-
-
-<?php
-}else{
-  ?>  
-  <p><a href="../paginas/validarPostulante.php" >Crear Postulacion</a></p>
-  <p><a href="../paginas/ventanaBuscarSolicitud.php" >Buscar Postulacion</a></p>
-
-<?php
-}
-?>
-</ul>
-
-
-
-<br><br>
-<a href=logout.php>Cerrar Sesion </a>
-</body>
+        <br><br>
+        <a href=logout.php>Cerrar Sesion </a>
+    </body>
 </html>
